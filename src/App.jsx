d26a44a5fc25e731a16d648bbd36a1eea3a59fa4
eternalbeginner <ini.dwiii@gmail.com>
2022-02-21 19:28:25 +0800

    Delete boiler plate and add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,29 @@
-import { useState } from 'react';
-
-import icons from './assets/icons.svg';
-import logoReact from './assets/logo-react.svg';
-import logoVite from './assets/logo-vite.svg';
-
-import './App.css';
-
-const links = {
-  github: 'https://github.com/eternalbeginner',
-  google: 'mailto:ini.dwiii@gmail.com',
-  react: 'https://reactjs.org',
-  vite: 'https://vitejs.dev',
-};
-
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  function activateLoading() {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }
-
   return (
-    <div className="app">
-      <img src={logoReact} className="app-bg" alt="react's logo" />
-      <img src={logoVite} className="app-logo" alt="vite's logo" />
-      <section className="app-content">
-        <h1 className="app-content-title">ViteReact</h1>
-        <span className="app-content-text">
-          <span>A </span>
-          <a href={links.react}>React</a>
-          <span>-based application boilerplate built with </span>
-          <a href={links.vite}>Vite</a>
-        </span>
+    <div className="calc">
+      <section className="calc-display">
+        <span className="calc-display-history" />
+        <span className="calc-display-input" />
       </section>
-      <section className="app-cta">
-        {isLoading ? (
-          <div className="app-cta-spinner" />
-        ) : (
-          <button
-            type="button"
-            className="app-button is-primary"
-            onClick={activateLoading}>
-            Click Me
-          </button>
-        )}
-      </section>
-      <section className="app-social">
-        <a href={links.github} className="app-button is-icon">
-          <svg>
-            <use xlinkHref={`${icons}#logo-github`} />
-          </svg>
-        </a>
-        <a href={links.google} className="app-button is-icon">
-          <svg>
-            <use xlinkHref={`${icons}#logo-google`} />
-          </svg>
-        </a>
+      <section className="calc-cta">
+        <button type="button">AC</button>
+        <button type="button">DEL</button>
+        <button type="button">%</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button">/</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">*</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">-</button>
+        <button type="button">0</button>
+        <button type="button">=</button>
+        <button type="button">+</button>
       </section>
     </div>
   );
