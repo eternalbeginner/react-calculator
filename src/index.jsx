5f47d69f4bce43024b7f65d4358fdc30c75abf2c
eternalbeginner <ini.dwiii@gmail.com>
2022-02-23 00:10:38 +0800

    Basic setup

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+render(
+	<StrictMode>
+		<h1>Hello from index!</h1>
+	</StrictMode>,
+	document.getElementById('root'),
+);
