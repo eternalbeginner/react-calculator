8efaac420ed7ca6e89b167f1266e81e2989a4d93
eternalbeginner <ini.dwiii@gmail.com>
2022-02-23 01:42:11 +0800

    Add resolver aliases configuration

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,7 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react({ include: './src' })],
+  plugins: [react({ include: './src' })],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, '../src/components'),
+      '@constants': resolve(__dirname, '../src/constants'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
 });
